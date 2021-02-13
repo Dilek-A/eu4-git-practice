e5f38544e54330d6ab5a6023410127ccee8c27d6
Dilek-A <dilekakcaytr@gmail.com>
2021-02-13 09:16:27 +0100

    more work on featureA on new branch

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -2,5 +2,6 @@ public class FeatureACode {
     public static void main(String[] args) {
 
         System.out.println("Awesome work for A started");
+        System.out.println("Eu4 is learning more git with review");
     }
 }
