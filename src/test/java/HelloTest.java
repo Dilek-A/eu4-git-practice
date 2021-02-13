a1206b4eb3c3f2128ef3f79e7a4624aee6072e6d
Dilek-A <dilekakcaytr@gmail.com>
2021-02-13 09:55:52 +0100

    master branch, spouse changed the movie location

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,6 @@
 public class HelloTest {
     public static void main(String[] args) {
-        System.out.println("Testing hello");
+        System.out.println("Testing hello for eu4");
 
         // Testing hello
 
